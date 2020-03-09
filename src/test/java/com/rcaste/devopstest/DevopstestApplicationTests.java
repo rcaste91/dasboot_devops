cfb7c7736c04b763f000232d0fd8f451b17ad1dc
Ronald Castellon <rcaste91@gmail.com>
2020-03-09 07:40:24 -0600

    agregando pruebas unitarias

--- src/test/java/com/rcaste/devopstest/DevopstestApplicationTests.java
+++ src/test/java/com/rcaste/devopstest/DevopstestApplicationTests.java
@@ -1,13 +1,21 @@
 package com.rcaste.devopstest;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.rcaste.devopstest.controller.HomeController;
+
+
 
 @SpringBootTest
 class DevopstestApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
-
+    public void testApp(){
+        HomeController hc = new HomeController();
+        String result = hc.home();
+        assertEquals(result, "Das Boot, reporting for duty! v. Jenkins");
+    }
 }
